86516c8df663b16241407c1f467beadd20e39991
fpetrit <f.petrit@outlook.fr>
2023-05-18 23:23:12 +0200

    correction fichier conf et ajout du root dir

--- conf/Conf.py
+++ conf/Conf.py
@@ -1,5 +1,17 @@
 import json
+from pathlib import Path
 
-__conf_file = open("config.json", 'r')
+# OPEN AND LOAD THE CONFIGURATION FILE
 
-cf = json.loads(__conf_file.read())
+cf_path = Path(__file__).parent.resolve() / "config.json"
+
+conf_file = open(cf_path, 'r')
+
+cf = json.loads(conf_file.read())
+
+
+# ADDING THE ROOT DIRECTORY PATH TO THE LOADED CONFIGURATION
+
+root_dir = Path(__file__).parents[1].resolve()
+
+cf["App"]["root_dir"] = root_dir
